a41f3f32e00b03ea28a6828fe6e854fae0d0a601
daniel Nguyen <danieltn91@gmail.com>
2019-11-06 21:46:28 -0800

    first commit

---
+++ GenerateBarCode/GenerateBarCode/MainForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace GenerateBarCode
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.picCode = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.printLabel = new System.Windows.Forms.Label();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picCode)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(310, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // picCode
+            // 
+            this.picCode.Location = new System.Drawing.Point(110, 61);
+            this.picCode.Name = "picCode";
+            this.picCode.Size = new System.Drawing.Size(275, 50);
+            this.picCode.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.picCode.TabIndex = 1;
+            this.picCode.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(89, 185);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "BarCode: ";
+            // 
+            // printLabel
+            // 
+            this.printLabel.AutoSize = true;
+            this.printLabel.Location = new System.Drawing.Point(227, 114);
+            this.printLabel.Name = "printLabel";
+            this.printLabel.Size = new System.Drawing.Size(0, 13);
+            this.printLabel.TabIndex = 3;
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(149, 182);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(155, 20);
+            this.txtCode.TabIndex = 4;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 293);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.printLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picCode);
+            this.Controls.Add(this.button1);
+            this.Name = "MainForm";
+            this.Text = "Generate BarCode";
+            ((System.ComponentModel.ISupportInitialize)(this.picCode)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox picCode;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label printLabel;
+        private System.Windows.Forms.TextBox txtCode;
+    }
+}
+
---
+++ GenerateBarCode/GenerateBarCode/MainForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GenerateBarCode
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string barCode = txtCode.Text;
+            try
+            {
+                Zen.Barcode.Code128BarcodeDraw brCode = Zen.Barcode.BarcodeDrawFactory.Code128WithChecksum;
+                picCode.Image = brCode.Draw(barCode, 60);
+                printLabel.Text = barCode;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("BarCode is not valid!");
+            }
+        }
+    }
+}
